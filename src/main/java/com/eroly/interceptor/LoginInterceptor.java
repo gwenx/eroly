be01aa00f648118225e7035b1f3de9c214aad898
gwenx <m18811106614@163.com>
2018-12-08 20:51:50 +0800

    修改登录拦截器为继承抽象方法

--- src/main/java/com/eroly/interceptor/LoginInterceptor.java
+++ src/main/java/com/eroly/interceptor/LoginInterceptor.java
@@ -4,13 +4,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
-
 import com.eroly.common.GlobalSt;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
-public class LoginInterceptor implements HandlerInterceptor{
-//	@Override
+public class LoginInterceptor extends HandlerInterceptorAdapter {
+	@Override
 	public boolean preHandle(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2) throws Exception {
 		HttpSession session = arg0.getSession();
 		String user = (String) session.getAttribute(GlobalSt.LOGIN_FLAG);
@@ -20,14 +18,5 @@ public class LoginInterceptor implements HandlerInterceptor{
 			arg1.sendRedirect("/eroly/public/gotoLogin");
 			return false;
 		}
-	}
-	public void afterCompletion(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, Exception arg3)
-			throws Exception {
-		// TODO Auto-generated method stub
-	}
-	public void postHandle(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, ModelAndView arg3)
-			throws Exception {
-		// TODO Auto-generated method stub
-		
 	}
 }
